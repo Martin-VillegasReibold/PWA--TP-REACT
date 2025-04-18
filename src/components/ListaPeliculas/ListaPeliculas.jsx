48c11a043b95c9ec24d1fbb428d368e3354e264e
JuanF368 <juanfarroni1314@gmail.com>
2025-04-18 20:33:35 -0300

    css para los contadores de genero y  cambio de texto por componente Titulo en listas

--- src/components/ListaPeliculas/ListaPeliculas.jsx
+++ src/components/ListaPeliculas/ListaPeliculas.jsx
@@ -1,20 +1,23 @@
 import Card from "../Card/Card";
+import Titulo from "../Titulo/Titulo";
+import styles from "./ListaPeliculas.module.css";
 
 const ListaPeliculas = ({tipo, peliculas}) => {
     
     const contadorGenero = peliculas.reduce((lista, pelicula) => {
         lista[pelicula.genre] = (lista[pelicula.genre] || 0)+1;
-        console.log(lista)
         return lista;
     }, {});
     return (
         <div>
-            <h1>{tipo}</h1>
-            {Object.keys(contadorGenero).map((genero) => {
-                return (
-                <h4 key={genero}>{genero}: {contadorGenero[genero]}</h4>
-                );
-            })}
+            <Titulo title={tipo}/>
+            <div className={styles.generosContainer}>
+                {Object.keys(contadorGenero).map((genero) => {
+                    return (
+                    <p key={genero}>{genero}: {contadorGenero[genero]}</p>
+                    );
+                })}
+            </div>
             <div className="movie-list">
                 {peliculas.length > 0 ?(
                     peliculas.map((pelicula, index) => {
